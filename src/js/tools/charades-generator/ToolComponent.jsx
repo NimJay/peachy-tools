3c24eb31ff442fb96e15ffd000ef5161be4d5d61
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2017-04-08 19:36:37 -0400

    Align only buttons to the right

--- src/js/tools/charades-generator/ToolComponent.jsx
+++ src/js/tools/charades-generator/ToolComponent.jsx
@@ -30,9 +30,11 @@ class ToolComponent extends React.Component {
             <section className="toolsection">
                 <div className="container">
                     <h1 style={{'minHeight': '2.5em'}}>{charade}</h1>
-                    <button
-                        type="button"
-                        onClick={this.onGenerate.bind(this)}>GENERATE</button>
+                    <div style={{'textAlign': 'right'}}>
+                        <button
+                            onClick={this.onGenerate.bind(this)}
+                            type="button">GENERATE</button>
+                    </div>
                 </div>
             </section>
         );
