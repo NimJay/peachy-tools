1ec6900fb5df493bc89a15742c96f8191ae01a50
Nim <nim@harplabs.com>
2017-04-15 13:02:10 -0400

    Introduce new Charade format

--- src/js/tools/charades-generator/Charade.js
+++ src/js/tools/charades-generator/Charade.js
@@ -1,9 +1,43 @@
-// A list of charades.
+/**
+ * A list of Charades.
+ */
 const CHARADES = [
-    "Inception", "Ski Lift", "Lion King", "Message In A Bottle",
-    "Cat Food", "Ice Age", "Sloth", "Pikachu", "Time Machine",
-    "Harry Potter", "Computer Bug", "Google Search", "Graduation",
-    "Gold Digger", "Broom"
+
+// Books
+{charade: "Harry Potter"},
+{charade: "Titanic"},
+
+// Creatures
+{charade: "Blue Whale"},
+{charade: "Pikachu"},
+{charade: "Sea Monster"},
+{charade: "Sloth"},
+
+// Movies
+{charade: "Inception"},
+{charade: "The Lion King"},
+{charade: "Ice Age"},
+
+// Shows
+{charade: "How I Met Your Mother"},
+
+// Music
+{charade: "Cheap Thrills"},
+{charade: "I Took A Pill In Ibiza"},
+
+// People
+{charade: "Albert Einstein"},
+
+// Other
+{charade: "Broom"},
+{charade: "Cat Food"},
+{charade: "Computer Bug"},
+{charade: "Gold Digger"},
+{charade: "Google Search"},
+{charade: "Graduation"},
+{charade: "Message In A Bottle"},
+{charade: "Ski Lift"},
+{charade: "Time Machine"}
 ];
 
 
@@ -16,7 +50,7 @@ let charades = []; // This array is used as a stack.
  */
 function getNextCharade() {
     if (charades.length == 0)
-        charades = CHARADES.slice(0); // Reload stack.
+        charades = CHARADES.map(c => c.charade).slice(0); // Reload stack.
     let index = Math.floor(Math.random() * charades.length); // Random index.
     return charades.splice(index, 1)[0]; // Return removal.
 }
