4e3e4351f7f554f949d98bbcee7ce58ee8f14bf5
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2017-04-25 19:43:19 -0400

    Introduce Node.js development server to close #3

---
+++ src/main.js
@@ -0,0 +1,28 @@
+/**
+ * This file starts up a Node.js Express web server.
+ * This is only used during development.
+ * During production, an Apache HTTP Server is used instead.
+ */
+
+const express = require('express');
+const PORT = 3003;
+var app = express();
+
+// Serve this directory.
+app.use(express.static(__dirname));
+
+// Send index.html for Tools.
+var sendIndex = (req, res) => {res.sendFile(__dirname + '/index.html');}
+app.get('/character-counter', sendIndex);
+app.get('/charades-generator', sendIndex);
+app.get('/random-number-generator', sendIndex);
+
+// Everything else: index.html with 404.
+app.use((req, res) => {
+    res.status(404);
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, function () {
+    console.log('Server Peachy Tools at: http://locahost:' + PORT);
+});
