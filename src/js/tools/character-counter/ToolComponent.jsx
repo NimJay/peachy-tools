924b4c030a8df396c22fb8ba248dda06afb65a00
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2017-04-30 21:32:44 -0400

    Implement "Character Counter" tool to close #1

--- src/js/tools/character-counter/ToolComponent.jsx
+++ src/js/tools/character-counter/ToolComponent.jsx
@@ -1,17 +1,55 @@
 import React from 'react'
+import Checkbox from '../../components/Checkbox.jsx'
+import Output from '../../components/Output.jsx'
 
 
 class ToolComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+            iteration: 0,
+            ignoreWhitespace: false
+        };
+    }
+
+
+    // State setters.
+    setText(e) {
+        this.setState(
+            {text: e.target.value, iteration: this.state.iteration + 1});
+    }
+    setIgnoreWhitespace(iw) {
+        this.setState(
+            {ignoreWhitespace: iw, iteration: this.state.iteration + 1});
+    }
+
+
     render() {
+
+        let { text, iteration, ignoreWhitespace } = this.state,
+            totalCharacters = text.length;
+
+        if (ignoreWhitespace)
+            totalCharacters = text.replace(/\s/g,'').length;
+
         return (
-            <section>
+            <main>
                 <div className="container">
-                    <h1>5 Characters</h1>
-                    <textarea></textarea>
-                    <p>2 Words</p>
-                    <p>1 Line</p>
+                    <form>
+                        <textarea value={text}
+                            onChange={this.setText.bind(this)}
+                            placeholder="Type your text here..."></textarea>
+                        <Output iteration={iteration}>
+                            {totalCharacters} characters
+                        </Output>
+                        <Checkbox value={ignoreWhitespace}
+                            label="Ignore whitespace"
+                            onChange={this.setIgnoreWhitespace.bind(this)} />
+                    </form>
                 </div>
-            </section>
+            </main>
         );
     }
 }
