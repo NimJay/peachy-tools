c7ac13be8ede19baa4726cfbe1b90a053aec256a
jayawar9 <nim.jayawardena@mail.utoronto.ca>
2017-05-10 23:04:31 -0400

    Implement and utilize class for dynamic <meta> tags

--- src/index.php
+++ src/index.php
@@ -3,38 +3,25 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta charset="utf-8">
-		<meta name="description" content="Every little tool you need, in one place.">
-		<meta name="author" content="Nim">
 
 		<!-- Mobile Browser Toolbar Color -->
 		<!-- Chrome, Firefox OS and Opera --><meta name="theme-color" content="#95dc5f">
 		<!-- Windows Phone --><meta name="msapplication-navbutton-color" content="#95dc5f">
 		<!-- iOS Safari --><meta name="apple-mobile-web-app-status-bar-style" content="#95dc5f">
 
-		<title>Peachy Tools</title>
+		<!-- Chrome "Add to Homescreen" -->
+		<meta name="mobile-web-app-capable" content="yes">
+		<link rel="icon" href="/img/favicon-192.png" sizes="192x192">
 
 		<!-- favicon.png -->
 		<link rel="shortcut icon" href="favicon.png" type="image/png">
 		<link rel="icon" href="favicon.png" type="image/png">
 
-		<!-- Chrome "Add to Homescreen" -->
-		<meta name="mobile-web-app-capable" content="yes">
-		<link rel="icon" href="/img/favicon-192.png" sizes="192x192">
-
-		<!-- Open Graph Protocol -->
-		<meta property="og:title" content="Peachy Tools" />
-		<meta property="og:type" content="website" />
-		<meta property="og:url" content="https://peachytools.com/" />
-		<meta property="og:image" content="https://peachytools.com/img/logo-card.png" />
-		<meta property="og:image:width" content="200" />
-		<meta property="og:image:height" content="200" />
-		<meta property="og:description" content="Every little tool you need, in one place." />
-
-		<!-- Twitter Card -->
-		<meta name="twitter:card" content="summary" />
-		<meta name="twitter:title" content="Peachy Tools" />
-		<meta name="twitter:description" content="Every little tool you need, in one place." />
-		<meta name="twitter:image" content="https://peachytools.com/img/logo-card.png" />
+<?php
+		// <meta> tags that rely on the requested URI.
+		include_once 'php/MetaTags.php';
+		echo MetaTags::get() . '\n';
+?>
 
 		<!-- I M P O R T -->
         <!-- Roboto Font --><link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
