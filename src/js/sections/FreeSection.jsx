a54b5c87c556e7b91987f64eb164b993a8f0304b
Nim <i.am.nim.jay@gmail.com>
2017-12-19 21:17:27 -0500

    Replace <b> with <em> for better semantics

--- src/js/sections/FreeSection.jsx
+++ src/js/sections/FreeSection.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const FreeSection = () => (
     <section>
         <div className='container'>
-            <h2>Peachy Tools is <b>Free</b></h2>
+            <h2>Peachy Tools is <em>Free</em></h2>
             <p>
                 Peachy Tools is non-profit.<br />
                 We will never sell ads. We plan to run on donations.
--- src/js/sections/OpenSourceSection.jsx
+++ src/js/sections/OpenSourceSection.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const OpenSourceSection = () => (
     <section>
         <div className='container'>
-            <h2>Peachy Tools is <b>Open-source</b></h2>
+            <h2>Peachy Tools is <em>Open-source</em></h2>
             <p>
                 "Open-source" means anyone can view, edit, fix, and/or add to Peachy Tools' code.
             </p>
