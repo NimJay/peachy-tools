2d3333cbee7e1819203f282066abc5d02bcde344
NimJay <nim.jayawardena@alum.utoronto.ca>
2021-07-16 19:37:25 -0400

    Use PORT env. variable or 8080 in server.js

--- src/js/server/server.js
+++ src/js/server/server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const buildPage = require('./build-page.js');
 
 const NON_TOOL_PATHS = [ '/', '/contribute' ];
-const PORT = 3003;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
