42a3e3263de6150e77587f74c856178eea6cee9a
NimJay <nim.jayawardena@alum.utoronto.ca>
2021-07-18 14:46:40 -0400

    Remove files needed for PHP deployment

--- src/js/code-generators/run-code-generators.js
+++ src/js/code-generators/run-code-generators.js
@@ -1,5 +1,3 @@
-const htaccessGenerator = require('./htaccess-generator.js');
 const sitemapXmlGenerator = require('./sitemap-xml-generator.js');
 
-htaccessGenerator.generateHtaccess();
 sitemapXmlGenerator.generateSitemapXml();
